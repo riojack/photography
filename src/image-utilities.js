5a549192deab7b8a3a3e04c9f4f6277a376ae510
AJ W <ayjay122@gmail.com>
2016-07-27 20:12:30 -0500

    Refactor image scaling, provide test coverage; re-integrate;
     * Actually, re-architected scaling completely.

--- src/image-utilities.js
+++ src/image-utilities.js
@@ -1,9 +1,53 @@
+const LARGE_SCALE = 0.9;
+const MID_SCALE = 0.7;
+const SMALL_SCALE = 0.4;
+
+let scale = {
+  LARGE_SCALE, MID_SCALE, SMALL_SCALE
+};
+
 function isTallImage(image) {
   return image.height > image.width;
 }
 
-export default {
-  isTallImage
+function nextScale(scale) {
+  let proceedingScale = MID_SCALE;
+
+  if (scale === MID_SCALE) {
+    proceedingScale = SMALL_SCALE;
+  }
+  else if (scale === SMALL_SCALE) {
+    proceedingScale = LARGE_SCALE;
+  }
+
+  return proceedingScale;
 }
 
-export {isTallImage};
+function nextScaleOfImage(scale, image) {
+  let targetScale = nextScale(scale);
+
+  return {
+    height: image.height * targetScale,
+    width: image.width * targetScale
+  };
+}
+
+function previousScale(scale) {
+  return nextScale(nextScale(scale));
+}
+
+export default {
+  isTallImage,
+  nextScale,
+  nextScaleOfImage,
+  previousScale,
+  scale
+}
+
+export {
+  isTallImage,
+  nextScale,
+  nextScaleOfImage,
+  previousScale,
+  scale
+};
--- test/image-utilities.spec.js
+++ test/image-utilities.spec.js
@@ -2,7 +2,7 @@ import {expect} from 'chai';
 import {stub} from 'sinon';
 import Chance from 'chance';
 
-import {isTallImage} from '../src/image-utilities';
+import {isTallImage, nextScale, nextScaleOfImage, previousScale, scale} from '../src/image-utilities';
 
 describe('Image Utilities tests', () => {
   let chance;
@@ -22,6 +22,75 @@ describe('Image Utilities tests', () => {
 
     it('should return false if image.height is less than image.width', () => {
       expect(isTallImage({height: 1, width: 2})).to.equal(false);
+    });
+  });
+
+  describe('nextScale utility', () => {
+    it('should give MID_SCALE as the scaling value following LARGE_SCALE', () => {
+      expect(nextScale(scale.LARGE_SCALE)).to.equal(scale.MID_SCALE);
+    });
+
+    it('should give SMALL_SCALE as the scaling value following MID_SCALE', () => {
+      expect(nextScale(scale.MID_SCALE)).to.equal(scale.SMALL_SCALE);
+    });
+
+    it('should give LARGE_SCALE as the scaling value following SMALL_SCALE', () => {
+      expect(nextScale(scale.SMALL_SCALE)).to.equal(scale.LARGE_SCALE);
+    });
+  });
+
+  describe('nextScaleOfImage utility', () => {
+    let image;
+
+    beforeEach('set up', () => {
+      image = {
+        height: 100,
+        width: 100
+      };
+    });
+
+    it('should scale image to MID_SCALE when scaling from LARGE_SCALE', () => {
+      let imageScaled = nextScaleOfImage(scale.LARGE_SCALE, image);
+      expect(imageScaled).to.eql({
+        height: image.height * scale.MID_SCALE,
+        width: image.width * scale.MID_SCALE
+      });
+
+      expect(imageScaled, 'Separate references').to.not.equal(image);
+    });
+
+    it('should scale image to SMALL_SCALE when scaling from MID_SCALE', () => {
+      let imageScaled = nextScaleOfImage(scale.MID_SCALE, image);
+      expect(imageScaled).to.eql({
+        height: image.height * scale.SMALL_SCALE,
+        width: image.width * scale.SMALL_SCALE
+      });
+
+      expect(imageScaled, 'Separate references').to.not.equal(image);
+    });
+
+    it('should scale image to LARGE_SCALE when scaling from SMALL_SCALE', () => {
+      let imageScaled = nextScaleOfImage(scale.SMALL_SCALE, image);
+      expect(imageScaled).to.eql({
+        height: image.height * scale.LARGE_SCALE,
+        width: image.width * scale.LARGE_SCALE
+      });
+
+      expect(imageScaled, 'Separate references').to.not.equal(image);
+    });
+  });
+
+  describe('previousScale utility', () => {
+    it('should give MID_SCALE as the scaling value following SMALL_SCALE', () => {
+      expect(previousScale(scale.SMALL_SCALE)).to.equal(scale.MID_SCALE);
+    });
+
+    it('should give SMALL_SCALE as the scaling value following LARGE_SCALE', () => {
+      expect(previousScale(scale.LARGE_SCALE)).to.equal(scale.SMALL_SCALE);
+    });
+
+    it('should give LARGE_SCALE as the scaling value prior to MID_SCALE', () => {
+      expect(previousScale(scale.MID_SCALE)).to.equal(scale.LARGE_SCALE);
     });
   });
 });
