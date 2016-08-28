9e888d4aaf63ec0242923af5c0cc62064ad75a55
AJ W <ayjay122@gmail.com>
2016-08-27 21:06:19 -0500

    Whitespace clean up.

--- src/Breadcrumbs.jsx
+++ src/Breadcrumbs.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 class Breadcrumbs extends React.Component {
-  componentDidMount() {
-    require('../sass/breadcrumbs.scss');
-  }
+    componentDidMount() {
+      require('../sass/breadcrumbs.scss');
+    }
 
   render() {
     return <ul className="breadcrumbs">
