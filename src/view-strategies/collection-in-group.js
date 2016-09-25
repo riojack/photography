ea381a1d87b65f0763f7b71d8fca01663c51241e
AJ W <ayjay122@gmail.com>
2016-09-25 17:15:02 -0500

    New view strategy.
     * Test that the new view strategy returns the group containing only the
     matching collection.

---
+++ src/view-strategies/collection-in-group.js
@@ -0,0 +1,19 @@
+class GroupInCollectionStrategy {
+  constructor(groups, collectionName) {
+    this.groups = groups;
+    this.collectionName = collectionName;
+  }
+
+  next() {
+    var localCollectionName = this.collectionName,
+      matchingGroup = this.groups.find(group => group.collections.find(collection => collection.collection === localCollectionName));
+
+    return [Object.assign(
+      {},
+      matchingGroup,
+      {collections: matchingGroup.collections.filter(collection => collection.collection === localCollectionName)}
+    )];
+  }
+}
+
+export default GroupInCollectionStrategy;
---
+++ test/view-strategies/collection-in-group.spec.js
@@ -0,0 +1,48 @@
+import {expect} from 'chai';
+import {stub} from 'sinon';
+import Chance from 'chance';
+
+import GroupInCollectionStrategy from '../../src/view-strategies/collection-in-group';
+
+function pickRandomGroupAndCollection(listOfGroups, chance) {
+  let randomGroup = chance.pickone(listOfGroups);
+  return [randomGroup, chance.pickone(randomGroup.collections)];
+}
+
+describe('Group in a collection sort strategy tests', () => {
+  let chance,
+    listOfGroups,
+    expectedGroup,
+    expectedCollection;
+
+  beforeEach('set up', () => {
+    chance = new Chance();
+    listOfGroups = [
+      {
+        group: 'group-one',
+        collections: chance.n(() => ({collection: chance.sentence()}), chance.integer({min: 3, max: 7}))
+      },
+      {
+        group: 'group-two',
+        collections: chance.n(() => ({collection: chance.sentence()}), chance.integer({min: 3, max: 7}))
+      },
+      {
+        group: 'group-three',
+        collections: chance.n(() => ({collection: chance.sentence()}), chance.integer({min: 3, max: 7}))
+      },
+    ];
+
+    [expectedGroup, expectedCollection] = pickRandomGroupAndCollection(listOfGroups, chance);
+  });
+
+  describe('when calling next()', () => {
+    it('should return the group containing only the collection that matches', () => {
+      let strategy = new GroupInCollectionStrategy(listOfGroups, expectedCollection.collection),
+        expectedExpectedGroup = Object.assign({}, expectedGroup, {collections: [expectedCollection]}),
+        group = strategy.next();
+
+      expect(group).to.eql([expectedExpectedGroup]);
+    });
+
+  });
+});
