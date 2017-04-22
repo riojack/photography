09c4b0688c9e3d17d4b81a94ec7dcde5d9f3cc0b
AJ W <ayjay122@gmail.com>
2017-04-22 11:28:48 -0500

    Add new sheet

---
+++ sheets/spring.js
@@ -0,0 +1,60 @@
+const data = {
+  group: 'Spring Season',
+  collections: [{
+    collection: 'Spring\'s Arrival 2017',
+    added: 'April 13, 2017',
+    time: 1492059600000,
+    items: [
+      {
+        "name": "9xDPMxdZRl10",
+        "image": "./deck/spring-2017-001/9xDPMxdZRl10.jpg",
+        "backgroundUrl": "./deck/spring-2017-001/9xDPMxdZRl10.jpg",
+        "backgroundPosition": {
+          "x": 0,
+          "y": 0
+        },
+        "height": 576,
+        "width": 1024,
+        "tags": ["retina"]
+      },
+      {
+        "name": "tqQ3Tcclt2wn",
+        "image": "./deck/spring-2017-001/tqQ3Tcclt2wn.jpg",
+        "backgroundUrl": "./deck/spring-2017-001/tqQ3Tcclt2wn.jpg",
+        "backgroundPosition": {
+          "x": 0,
+          "y": 0
+        },
+        "height": 576,
+        "width": 1024,
+        "tags": ["retina"]
+      },
+      {
+        "name": "xsrOW8EaKwLK",
+        "image": "./deck/spring-2017-001/xsrOW8EaKwLK.jpg",
+        "backgroundUrl": "./deck/spring-2017-001/xsrOW8EaKwLK.jpg",
+        "backgroundPosition": {
+          "x": 0,
+          "y": 0
+        },
+        "height": 576,
+        "width": 1024,
+        "tags": ["retina"]
+      },
+      {
+        "name": "rYgoN4H4GtA8",
+        "image": "./deck/spring-2017-001/rYgoN4H4GtA8.jpg",
+        "backgroundUrl": "./deck/spring-2017-001/rYgoN4H4GtA8.jpg",
+        "backgroundPosition": {
+          "x": 0,
+          "y": 0
+        },
+        "height": 576,
+        "width": 1024,
+        "tags": ["retina"]
+      }
+    ]
+  }]
+};
+
+export {data as SpringsArrival};
