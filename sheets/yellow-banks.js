067ce19e98691946ed9a163503515ad6400470c7
riojack <ayjay122@gmail.com>
2018-08-15 20:00:54 -0500

    Add new photos

---
+++ sheets/yellow-banks.js
@@ -0,0 +1,48 @@
+const data = {
+  group: 'Yellow Banks',
+  collections: [{
+    collection: 'Summer Forest',
+    added: 'August 12, 2018',
+    time: 1534078800 * 1000,
+    items: [
+      {
+        "name": "EHAPgBFOOgW0TqfR",
+        "image": "./deck/yellow-banks-001/EHAPgBFOOgW0TqfR.jpg",
+        "backgroundUrl": "./deck/yellow-banks-001/EHAPgBFOOgW0TqfR.jpg",
+        "backgroundPosition": {
+          "x": 0,
+          "y": 0
+        },
+        "height": 576,
+        "width": 1024,
+        "tags": ["retina", "hero"]
+      },
+      {
+        "name": "UYMrvmWARfI00RMK",
+        "image": "./deck/yellow-banks-001/UYMrvmWARfI00RMK.jpg",
+        "backgroundUrl": "./deck/yellow-banks-001/UYMrvmWARfI00RMK.jpg",
+        "backgroundPosition": {
+          "x": 0,
+          "y": 0
+        },
+        "height": 576,
+        "width": 1024,
+        "tags": ["retina"]
+      },
+      {
+        "name": "FskiUTElpQbiNnbn",
+        "image": "./deck/yellow-banks-001/FskiUTElpQbiNnbn.jpg",
+        "backgroundUrl": "./deck/yellow-banks-001/FskiUTElpQbiNnbn.jpg",
+        "backgroundPosition": {
+          "x": 0,
+          "y": 0
+        },
+        "height": 576,
+        "width": 1024,
+        "tags": ["retina"]
+      }
+    ]
+  }]
+};
+
+export {data as YellowBanks};
