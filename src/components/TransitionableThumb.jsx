1923eb6eb0edd135f3a78767ac7f74ab06a0f350
riojack <ayjay122@gmail.com>
2018-08-16 08:54:55 -0500

    Start building the transitional thumb

---
+++ src/components/TransitionableThumb.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class TransitionableThumb extends React.Component {
+  render() {
+    return <div>
+      <div className="left-shark"/>
+      <div className="right-shark"/>
+    </div>;
+  }
+}
+
+export default TransitionableThumb;
