cf2079fd6b7d2b378af67158fbef0e19f60344a8
riojack <ayjay122@gmail.com>
2018-09-03 18:48:19 -0500

    New photos

--- sheets/jester.js
+++ sheets/jester.js
@@ -16,6 +16,73 @@ const data = {
         "height": 576,
         "width": 1024
       }]
+  },
+  {
+    collection: 'Jester Park',
+    added: 'August 30, 2018',
+    time: 1535623200000,
+    items: [
+      {
+        name: 'dOSGlzviN0mVnZtx',
+        "image": "./deck/jester-002/dOSGlzviN0mVnZtx.jpg",
+        "backgroundUrl": "./deck/jester-002/dOSGlzviN0mVnZtx.jpg",
+        "backgroundPosition": {
+          "x": 0,
+          "y": 0
+        },
+        "height": 576,
+        "width": 1024,
+        "tags": ["retina", "hero"]
+      },
+      {
+        name: 'knHgDiqSOltesbQh',
+        "image": "./deck/jester-002/knHgDiqSOltesbQh.jpg",
+        "backgroundUrl": "./deck/jester-002/knHgDiqSOltesbQh.jpg",
+        "backgroundPosition": {
+          "x": 0,
+          "y": 0
+        },
+        "height": 576,
+        "width": 1024,
+        "tags": ["retina"]
+      },
+      {
+        name: 'PpZEGCBnuvHYOquu',
+        "image": "./deck/jester-002/PpZEGCBnuvHYOquu.jpg",
+        "backgroundUrl": "./deck/jester-002/PpZEGCBnuvHYOquu.jpg",
+        "backgroundPosition": {
+          "x": 0,
+          "y": 0
+        },
+        "height": 576,
+        "width": 1024,
+        "tags": ["retina"]
+      },
+      {
+        name: 'vKPHqNoOHyuxxSWi',
+        "image": "./deck/jester-002/vKPHqNoOHyuxxSWi.jpg",
+        "backgroundUrl": "./deck/jester-002/vKPHqNoOHyuxxSWi.jpg",
+        "backgroundPosition": {
+          "x": 0,
+          "y": 0
+        },
+        "height": 576,
+        "width": 1024,
+        "tags": ["retina"]
+      },
+      {
+        name: 'zSXyBBuHtIZtw0CT',
+        "image": "./deck/jester-002/zSXyBBuHtIZtw0CT.jpg",
+        "backgroundUrl": "./deck/jester-002/zSXyBBuHtIZtw0CT.jpg",
+        "backgroundPosition": {
+          "x": 0,
+          "y": 0
+        },
+        "height": 576,
+        "width": 1024,
+        "tags": ["retina"]
+      },
+    ]
   }]
 };
 
