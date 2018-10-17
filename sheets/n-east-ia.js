db21a01ee5208f8fa36fdc7329e9825536475ce1
riojack <ayjay122@gmail.com>
2018-10-17 18:30:12 -0500

    Fix css, add new photos

---
+++ sheets/n-east-ia.js
@@ -0,0 +1,30 @@
+const data = {
+  group: 'Northeast Iowa',
+  collections: [{
+    collection: 'Mississippi',
+    added: 'October 9, 2018',
+    time: 1539090000 * 1000,
+    items: [
+      { "name": "ei7lmB06kuPL6GoO", "image": "./deck/n-east-ia-001/ei7lmB06kuPL6GoO.jpg", "backgroundUrl": "./deck/n-east-ia-001/ei7lmB06kuPL6GoO.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 576, "width": 1024, "tags": ["retina", "hero"], "categories": [] },
+      { "name": "QG1M0TMxe3Wlpfy9", "image": "./deck/n-east-ia-001/QG1M0TMxe3Wlpfy9.jpg", "backgroundUrl": "./deck/n-east-ia-001/QG1M0TMxe3Wlpfy9.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 576, "width": 1024, "tags": ["retina"], "categories": [] },
+      { "name": "dYgKwufrgYiVSPzV", "image": "./deck/n-east-ia-001/dYgKwufrgYiVSPzV.jpg", "backgroundUrl": "./deck/n-east-ia-001/dYgKwufrgYiVSPzV.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 576, "width": 1024, "tags": ["retina"], "categories": [] },
+      { "name": "rcUwH7sTPXNteKkv", "image": "./deck/n-east-ia-001/rcUwH7sTPXNteKkv.jpg", "backgroundUrl": "./deck/n-east-ia-001/rcUwH7sTPXNteKkv.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 1024, "width": 576, "tags": ["retina", "portrait"], "categories": [] },
+      { "name": "GWa1YqkZidClckHk", "image": "./deck/n-east-ia-001/GWa1YqkZidClckHk.jpg", "backgroundUrl": "./deck/n-east-ia-001/GWa1YqkZidClckHk.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 576, "width": 1024, "tags": ["retina"], "categories": [] },
+      { "name": "J6b7PBaRy7b3GdA6", "image": "./deck/n-east-ia-001/J6b7PBaRy7b3GdA6.jpg", "backgroundUrl": "./deck/n-east-ia-001/J6b7PBaRy7b3GdA6.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 576, "width": 1024, "tags": ["retina"], "categories": [] },
+      { "name": "tiO5v2OsOXEhPGZu", "image": "./deck/n-east-ia-001/tiO5v2OsOXEhPGZu.jpg", "backgroundUrl": "./deck/n-east-ia-001/tiO5v2OsOXEhPGZu.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 576, "width": 1024, "tags": ["retina"], "categories": [] },
+      { "name": "t92TUYs80zhU9gY4", "image": "./deck/n-east-ia-001/t92TUYs80zhU9gY4.jpg", "backgroundUrl": "./deck/n-east-ia-001/t92TUYs80zhU9gY4.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 576, "width": 1024, "tags": ["retina"], "categories": [] },
+      { "name": "v0rHl9Kg2ThHK9cA", "image": "./deck/n-east-ia-001/v0rHl9Kg2ThHK9cA.jpg", "backgroundUrl": "./deck/n-east-ia-001/v0rHl9Kg2ThHK9cA.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 576, "width": 1024, "tags": ["retina"], "categories": [] },
+      { "name": "Kc4QfiePiP3oud0D", "image": "./deck/n-east-ia-001/Kc4QfiePiP3oud0D.jpg", "backgroundUrl": "./deck/n-east-ia-001/Kc4QfiePiP3oud0D.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 576, "width": 1024, "tags": ["retina"], "categories": [] },
+      { "name": "QaBVAwEvsfJlD0uN", "image": "./deck/n-east-ia-001/QaBVAwEvsfJlD0uN.jpg", "backgroundUrl": "./deck/n-east-ia-001/QaBVAwEvsfJlD0uN.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 576, "width": 1024, "tags": ["retina"], "categories": [] },
+      { "name": "xYqAsm1perZRltwa", "image": "./deck/n-east-ia-001/xYqAsm1perZRltwa.jpg", "backgroundUrl": "./deck/n-east-ia-001/xYqAsm1perZRltwa.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 576, "width": 1024, "tags": ["retina"], "categories": [] },
+      { "name": "d9cfJBAWs75Uk8li", "image": "./deck/n-east-ia-001/d9cfJBAWs75Uk8li.jpg", "backgroundUrl": "./deck/n-east-ia-001/d9cfJBAWs75Uk8li.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 1024, "width": 576, "tags": ["retina", "portrait"], "categories": [] },
+      { "name": "fhybimMt826ClNAB", "image": "./deck/n-east-ia-001/fhybimMt826ClNAB.jpg", "backgroundUrl": "./deck/n-east-ia-001/fhybimMt826ClNAB.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 576, "width": 1024, "tags": ["retina"], "categories": [] },
+      { "name": "vRZ3twR9WQtuhqg3", "image": "./deck/n-east-ia-001/vRZ3twR9WQtuhqg3.jpg", "backgroundUrl": "./deck/n-east-ia-001/vRZ3twR9WQtuhqg3.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 576, "width": 1024, "tags": ["retina"], "categories": [] },
+      { "name": "wwJgfYNbWQ7KC7RA", "image": "./deck/n-east-ia-001/wwJgfYNbWQ7KC7RA.jpg", "backgroundUrl": "./deck/n-east-ia-001/wwJgfYNbWQ7KC7RA.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 576, "width": 1024, "tags": ["retina"], "categories": [] },
+      { "name": "xnKWfG1HQLvH4jPe", "image": "./deck/n-east-ia-001/xnKWfG1HQLvH4jPe.jpg", "backgroundUrl": "./deck/n-east-ia-001/xnKWfG1HQLvH4jPe.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 1024, "width": 576, "tags": ["retina", "portrait"], "categories": [] },
+      { "name": "SQuYzrEc2SOGHWgl", "image": "./deck/n-east-ia-001/SQuYzrEc2SOGHWgl.jpg", "backgroundUrl": "./deck/n-east-ia-001/SQuYzrEc2SOGHWgl.jpg", "backgroundPosition": { "x": 0, "y": 0 }, "height": 576, "width": 1024, "tags": ["retina"], "categories": [] }
+    ]
+  }]
+};
+
+export { data as NortheastIowa };
