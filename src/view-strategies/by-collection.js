403e70badcb220b59157d962d483588835b1e064
AJ Whatling <riojack@users.noreply.github.com>
2018-10-17 20:32:54 -0500

    start ByCollectionStrategy

---
+++ src/view-strategies/by-collection.js
@@ -0,0 +1,51 @@
+class ByCollectionStrategy {
+  static create(groups) {
+    return new ByCollectionStrategy(groups);
+  }
+
+  constructor(groups) {
+    this.groups = groups;
+    this.nextCalls = 0;
+  }
+
+  reset() {}
+
+  next() {
+    this.nextCalls++;
+
+    let collectionCount = 0;
+
+    const groupsToRender = [];
+
+    for (let i = 0; i < this.groups.length; i++) {
+      const groupCollections = this.groups[i].collections;
+      const groupShallowCopy = Object.assign({}, this.groups[i], {collections: []});
+
+      let exitedEarly = false;
+      let j = 0;
+
+      for (; j < groupCollections.length; j++) {
+        const collection = groupCollections[j];
+
+        groupShallowCopy.collections.push(collection);
+
+        collectionCount++;
+
+        if (collectionCount === this.nextCalls) {
+          exitedEarly = true;
+          break;
+        }
+      }
+      groupsToRender.push(groupShallowCopy);
+
+      if (exitedEarly) {
+        break;
+      }
+    }
+
+    return groupsToRender;
+  }
+
+}
+
+export default ByCollectionStrategy;
