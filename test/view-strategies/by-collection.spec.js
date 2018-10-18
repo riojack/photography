ee99c34d236ddcd02220b2b2dce1f14bf123bbf7
riojack <ayjay122@gmail.com>
2018-10-17 20:32:54 -0500

    start ByCollectionStrategy

---
+++ test/view-strategies/by-collection.spec.js
@@ -0,0 +1,52 @@
+import { expect } from 'chai';
+
+import ByCollectionStrategy from '../../src/view-strategies/by-collection';
+
+describe('ByCollectionStrategy tests', () => {
+  const GROUP_1 = { group: 'Group 1', collections: [{ collection: 'G1 Collection 1', items: [] }] };
+  const GROUP_2 = {
+    group: 'Group 2', collections: [
+      { collection: 'G2 Collection 1', items: [] },
+      { collection: 'G2 Collection 2', items: [] },
+      { collection: 'G2 Collection 3', items: [] }
+    ]
+  };
+
+  const GROUPS = [GROUP_1, GROUP_2];
+
+  it('should return collections one at a time', () => {
+    const strat = new ByCollectionStrategy(GROUPS);
+
+    expect(strat.next())
+      .to.eql([{ group: 'Group 1', collections: [{ collection: 'G1 Collection 1', items: [] }] }]);
+
+    expect(strat.next())
+      .to.eql([
+        { group: 'Group 1', collections: [{ collection: 'G1 Collection 1', items: [] }] },
+        { group: 'Group 2', collections: [{ collection: 'G2 Collection 1', items: [] }] }
+      ]);
+
+    expect(strat.next())
+      .to.eql([
+        { group: 'Group 1', collections: [{ collection: 'G1 Collection 1', items: [] }] },
+        {
+          group: 'Group 2', collections: [
+            { collection: 'G2 Collection 1', items: [] },
+            { collection: 'G2 Collection 2', items: [] }
+          ]
+        }
+      ]);
+
+    expect(strat.next())
+      .to.eql([
+        { group: 'Group 1', collections: [{ collection: 'G1 Collection 1', items: [] }] },
+        {
+          group: 'Group 2', collections: [
+            { collection: 'G2 Collection 1', items: [] },
+            { collection: 'G2 Collection 2', items: [] },
+            { collection: 'G2 Collection 3', items: [] },
+          ]
+        }
+      ]);
+  });
+});
