ccb6b35feb2b98de6f16e8cd0081d1afe70568c4
riojack <ayjay122@gmail.com>
2018-10-18 17:09:02 -0500

    Confidence test that calling past the end of the groups only returns exactly the number of groups and no more

--- test/view-strategies/by-collection.spec.js
+++ test/view-strategies/by-collection.spec.js
@@ -49,4 +49,29 @@ describe('ByCollectionStrategy tests', () => {
         }
       ]);
   });
+
+  it('should return all groups if next is called again after returning all collections already', () => {
+    const strat = new ByCollectionStrategy(GROUPS);
+
+    strat.next();
+    strat.next();
+    strat.next();
+    strat.next();
+    strat.next();
+    strat.next();
+    strat.next();
+    strat.next();
+
+    expect(strat.next())
+      .to.eql([
+        { group: 'Group 1', collections: [{ collection: 'G1 Collection 1', items: [] }] },
+        {
+          group: 'Group 2', collections: [
+            { collection: 'G2 Collection 1', items: [] },
+            { collection: 'G2 Collection 2', items: [] },
+            { collection: 'G2 Collection 3', items: [] },
+          ]
+        }
+      ]);
+  });
 });
